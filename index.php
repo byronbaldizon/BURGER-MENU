7930acbbc0af3cead8efd1bb64ab859302cdf24a
byron <robertodelfosnet@gmail.com>
2022-01-03 23:57:21 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!--REF. https://actiontoys.es/shop/page/7/-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>KOLECTCO</title>
+	<link rel="stylesheet" href="styles.css">
+	<!--FONTAWESOME-->
+	<script src="https://kit.fontawesome.com/9cb08368a2.js" crossorigin="anonymous"></script>
+	<script src="app.js" defer></script>
+</head>
+<body>
+	<div class="main-container">
+		
+			
+		
+		<!--*************************************-->
+		<!--             SECTION 1               -->
+		<!--*************************************-->
+		             <!--respmenu-->
+		<nav class="menu">
+			<label class="logo">KolectCO</label>
+			<ul class="menu-items">
+				<li class="active"><a href="">Inicio</a></li>
+				<li><a href="">Acerca</a></li>
+				<li><a href="">Servicios</a></li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+			<span class="btn-menu">
+				<i class="fas fa-bars"></i>
+			</span>	
+		</nav>
+		
+		        
+
+		
+		
+	</div><!--end Main Container-->
+</body>
+</html>
